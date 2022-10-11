29e01de7d1d0f3fc29bf6d5b0ab23f572560f3ff
roman-thapa <romanthapa7795@gmail.com>
2022-10-11 16:00:25 +0530

    Make empty squares inside container

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-continer = document.querySelector('.continer');
+let numberOfSquare = 16;
 
-for(i=0; i<=16; i++){
+container = document.querySelector('.container');
+
+for(i=0; i<=numberOfSquare; i++){
     let arrange = document.createElement('div');
     arrange.className = 'arrange';
-    for (j=0; j<=16; j++){
+    for (j=0; j<=numberOfSquare; j++){
         let square = document.createElement("div");
         square.className = 'square';
-        square.innerText = i;
-
+        square.style.height = "calc(500px / 16";
+        square.style.width = "calc(500px / 16";
         arrange.appendChild(square);
-        
     }
-    continer.appendChild(arrange);
+    container.appendChild(arrange);
 }
